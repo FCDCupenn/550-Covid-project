9f3151ae1d432ce1c785dc395ba0a0d5a00abff1
Yilin Wang <yw510@cantab.ac.uk>
2024-07-25 15:20:58 +0800

    add marker and test data to map and homepage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,8 +4,11 @@ import MapComponent from '../components/MapComponent';
 
 export default function HomePage() {
   const testData = [
-    { lat: 51.505, long: -0.09, cases: 10000, deaths: 500 },
-    { lat: 48.8566, long: 2.3522, cases: 20000, deaths: 1000 }
+    { "country": "United States", "lat": 37.0902, "long": -95.7129, "cases": 50000000, "deaths": 800000 },
+    { "country": "Brazil", "lat": -14.2350, "long": -51.9253, "cases": 22000000, "deaths": 610000 },
+    { "country": "India", "lat": 20.5937, "long": 78.9629, "cases": 34000000, "deaths": 450000 },
+    { "country": "France", "lat": 46.2276, "long": 2.2137, "cases": 7000000, "deaths": 110000 },
+    { "country": "United Kingdom", "lat": 55.3781, "long": -3.4360, "cases": 10000000, "deaths": 130000 }
   ];
 
   return (
