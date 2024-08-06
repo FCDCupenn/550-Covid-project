447d08ba247452509577efcf701be451ba323456
Wenbin-Hong <122278331+Wenbin-Hong@users.noreply.github.com>
2024-08-06 15:31:50 +0800

    Improve Vaccination page

--- client/src/pages/VaccinationDataPage.js
+++ client/src/pages/VaccinationDataPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import * as echarts from 'echarts';
-import { FormControl, InputLabel, Select, MenuItem, TextField, Box } from '@mui/material';
+import { FormControl, InputLabel, Select, MenuItem, TextField, Box, Card, CardContent, Typography, Button, Grid } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers-pro/LocalizationProvider';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
@@ -9,6 +9,37 @@ import isBetween from 'dayjs/plugin/isBetween';
 const config = require('../config.json');
 
 dayjs.extend(isBetween);
+
+const infoCards = [
+  {
+    title: 'PMC Vaccines Intro',
+    description: 'Different types of vaccines are designed to stimulate your immune system and fight against the novel coronavirus. Based on the best approach and technology available in the production of vaccines, scientists will determine their type..',
+    buttonText: 'Check Here',
+    link: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8426786/',
+    icon: '📊'
+  },
+  {
+    title: 'Global Vaccine Action Plan',
+    description: 'The Global Vaccine Action Plan (GVAP) ― endorsed by the 194 Member States of the World Health Assembly in May 2012 ― is a framework to prevent millions of deaths by 2020 through more equitable access to existing vaccines for people in all communities..',
+    buttonText: 'Know More',
+    link: 'https://www.who.int/teams/immunization-vaccines-and-biologicals/strategies/global-vaccine-action-plan',
+    icon: '💉'
+  },
+  {
+    title: 'What You Need To Know',
+    description: 'The COVID-19 vaccine is very good at preventing serious illness, hospitalization and death. Because the virus that causes COVID-19 continues to change, vaccines are updated to help fight the disease.',
+    buttonText: 'See It',
+    link: 'https://www.hopkinsmedicine.org/health/conditions-and-diseases/coronavirus/covid-19-vaccine-what-you-need-to-know',
+    icon: '💉'
+  },
+  {
+    title: 'Register with Us',
+    description: 'Stay connected and receive the latest updates and information about COVID-19. Register with us to get personalized alerts and resources to keep you and your community safe. We will provide all the information matters',
+    buttonText: 'Register now',
+    link: '/login',
+    icon: '📝'
+  }
+];
 
 const VaccinationDataPage = () => {
   const [data, setData] = useState([]);
@@ -87,6 +118,10 @@ const VaccinationDataPage = () => {
   };
 
   useEffect(() => {
+    const chartDom = document.getElementById('main');
+    const myChart = echarts.init(chartDom);
+    myChart.setOption(getOptions([])); // Set an initial empty chart configuration
+
     fetch(`http://${config.server_host}:${config.server_port}/vaccination_date`)
       .then(res => res.json())
       .then(resText => {
@@ -97,8 +132,6 @@ const VaccinationDataPage = () => {
         setData(resText);
         const countryName = [...new Set(data?.map(v => v.country_name))];
         setCountryName(countryName);
-        const chartDom = document.getElementById('main');
-        const myChart = echarts.init(chartDom);
         setParams(prevParams => ({
           ...prevParams,
           name: countryName[0]
@@ -182,6 +215,34 @@ const VaccinationDataPage = () => {
         </div>
       </div>
       <div id="main" style={{ height: '500px', width: '95%', margin: '0 auto' }}></div>
+      <div style={{ width: '95%', margin: '20px auto' }}>
+        <Typography variant="h5" component="h2" style={{ marginBottom: '20px' }}>
+          Related Websites
+        </Typography>
+        <Grid container spacing={2}>
+          {infoCards.map((card, index) => (
+            <Grid item xs={12} sm={6} md={3} key={index}>
+              <Card style={{ height: '100%' }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {card.icon} {card.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" style={{ margin: '10px 0' }}>
+                    {card.description}
+                    </Typography>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => window.open(card.link, '_blank')}
+                  >
+                    {card.buttonText}
+                  </Button>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </div>
   );
 };
